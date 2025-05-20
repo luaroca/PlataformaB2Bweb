b1810dd64d7050ba93394ed78e5cf521b77a45e3
Bryan-Guerra <xboxinc@gmail.com>
2025-05-20 10:18:48 -0500

    detalle producto code mod

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '12345',
+  password: 'mysql20',
   database: 'provecta'
 });
 
